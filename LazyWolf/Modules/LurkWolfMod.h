d7937a387ccf347fcd94b1ae58c3f514cfb76c6e
NowlasWolf <38125683+NowlasWolf@users.noreply.github.com>
2020-02-11 13:21:33 -0800

    Started adding thread safety
    Doesn't really work yet

---
+++ LazyWolf/Modules/LurkWolfMod.h
@@ -0,0 +1,88 @@
+#include"../lazymod.h"
+#include<ncurses.h>
+#include<thread>
+#include<mutex>
+#include<vector>
+#include<stdio.h>
+#include<unistd.h>
+#include<cstring>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/ip.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+
+
+class LurkWolf : public LazyModule{
+    protected:
+        WINDOW *header;
+	    WINDOW *log;
+	    WINDOW *pstatus;
+        WINDOW *extrawindow1;
+        WINDOW *extrawindow2;
+
+
+        void program(){
+            header = newwin(2,x_size,2,0);
+	        log = newwin(y_size-6, x_size, 4, 0);
+	        pstatus = newwin(2,x_size,y_size-4,0);
+
+            int log_x, log_y;
+            getmaxyx(log, log_y, log_x);
+            extrawindow1 = newwin(log_y/2-1, 35, 2, x_size-35);
+	        extrawindow2 = newwin(log_y/2+(log_y%2)+1, 35, log_y/2+1, x_size-35);
+            scrollok(log, true);
+
+            updateWindows();
+            mvwprintw(log, log_y, 0, "LurkWolf says Hi!");
+            while(!stopped){
+                if(buf[0] != 0){
+                    wprintw(log, ">%s",buf);
+                    buf[0] = 0;
+                    if(inFocus)updateWindows();
+                }
+            }
+
+        }
+
+        void updateWindows(){
+            wrefresh(header);
+            wrefresh(log);
+            wrefresh(pstatus);
+            wrefresh(extrawindow1);
+            wrefresh(extrawindow2);
+        }
+        /*
+        update_header(){
+            
+        }
+        update_pstatus(){
+
+        }
+        update_extrawin(){
+
+        }
+        */
+    public:
+        LurkWolf(modDimensions d)
+            :LazyModule(d){
+            win = newwin(d.y_size, d.x_size, d.y, d.x);
+            x = d.x;
+            y = d.y;
+            x_size = d.x_size;
+            y_size = d.y_size;
+            scrollok(win, true);
+            strncpy(name, "LurkWolf", 10);
+            
+        }
+
+        void sendModInput(char* buffer){
+            mlock.lock();
+            strcpy(buf, buffer);
+            mlock.unlock();
+        }
+        char* getName(){
+            return name;
+        }
+};
---
+++ LazyWolf/lazywolf.h
@@ -0,0 +1,205 @@
+#ifndef LAZYWOLF_H
+#define LAZYWOLF_H
+
+#include<ncurses.h>
+#include"lazymod.h"
+
+struct lwItems{
+    std::vector<LazyModule*>* modules;
+    WINDOW* header;
+    WINDOW* input;
+    WINDOW* status;
+    int* modfocus;
+};
+
+void handle_signal(int signal){
+    if (signal == SIGINT){
+      endwin();
+      printf("Closing Nicely\n");
+      exit(0);	
+    }
+}
+
+WINDOW *init_header(int ysize, int xsize, int y, int x){
+    WINDOW *win = newwin(ysize , xsize, y, x);
+    wattron(win,A_STANDOUT);
+    wmove(win,0,0);
+    for(int i = 0; i < xsize*2; i++){
+       wprintw(win," ");
+    }
+
+    mvwprintw(win,0,0,"LazyWolf Modules Manager");
+    wattroff(win, A_STANDOUT);
+    wnoutrefresh(win);
+    return win;
+}
+
+void updateHeader(WINDOW* win, std::vector<LazyModule*> list, int focus){
+    int x, y;
+    getmaxyx(win, y, x);
+    wattron(win,A_STANDOUT);
+    wmove(win,0,0);
+    for(int i = 0; i < x*2; i++){
+       wprintw(win," ");
+    }
+
+    mvwprintw(win,0,0,"LazyWolf Modules Manager");
+    wmove(win, 1, 0);
+
+    int width = x/list.size();
+    for(int i = 0; i < list.size(); i++){
+      int pos = ((width/2)-5) + width*i;
+      if(i == focus) wattroff(win, A_STANDOUT);
+      mvwprintw(win, 1, pos, "%s",list[i]->getName());
+      wattron(win,A_STANDOUT);
+    }
+    wattroff(win, A_STANDOUT);
+    //wnoutrefresh(win);
+    wrefresh(win);
+}
+
+WINDOW *init_input(int ysize, int xsize, int y, int x){
+    WINDOW *win = newwin(ysize , xsize, y, x);
+    return win;
+}
+
+WINDOW *init_status(int ysize, int xsize, int y, int x){
+    WINDOW *win = newwin(ysize , xsize, y, x);
+    wattron(win,A_STANDOUT);
+    wmove(win,0,0);
+    for(int i = 0; i < xsize; i++){
+       wprintw(win," ");
+    }
+    mvwprintw(win,0,0,"LazyWolf started!");
+    wnoutrefresh(win);
+    return win;
+}
+
+void updateStatus(WINDOW* win){
+    int line_x, line_y, win_x, win_y;
+    getmaxyx(win, win_y, win_x);
+    getyx(win, line_y, line_x);
+    int pos = (win_x/2)-(line_x/2);
+    wmove(win,0,0);
+    for(int i = 0; i < 1; i++){
+        wprintw(win, " ");
+    }
+    wnoutrefresh(win);
+}
+
+void redrawStatus(WINDOW* win){
+    int y, x;
+    getmaxyx(win, y, x);
+    wmove(win,0,0);
+    for(int i = 0; i < x; i++){
+       wprintw(win," ");
+    }
+    wnoutrefresh(win);
+}
+
+void statusPrint(WINDOW* win, const char* message){
+    int win_x, win_y;
+    getmaxyx(win, win_y, win_x);
+    int pos = (win_x/2)-(strlen(message)/2);
+    wmove(win,0,0);
+    for(int i = 0; i < pos; i++){
+        wprintw(win, " ");
+    }
+    wprintw(win,"%s",message);
+    for(int i = 0; i < win_x-pos-strlen(message); i++){
+        wprintw(win, " ");
+    }
+    wnoutrefresh(win);
+}
+
+int changeFocus(std::vector<LazyModule*> list, int cfocus, int newfocus){
+    list[cfocus]->setFocus(false);
+    if(newfocus >= list.size()){
+        newfocus = 0;
+    }else if(newfocus < 0){
+        newfocus = list.size()-1;
+    }
+    list[newfocus]->setFocus(true);
+    list[newfocus]->redrawMod();
+
+    return newfocus;
+}
+
+void loadModule(LazyModule* mod, struct lwItems stuff){
+    char buf[100];
+    std::vector<LazyModule*> temp = *stuff.modules;
+
+    stuff.modules->push_back(mod);
+    mod->runMod();
+    mod->refreshMod();
+    *stuff.modfocus = changeFocus(*stuff.modules, *stuff.modfocus, stuff.modules->size()+1);
+
+    sprintf(buf, "%s module loaded", mod->getName());
+    statusPrint(stuff.status, buf);
+}
+
+
+
+static void readline(WINDOW* win, char *buffer, int buflen)
+/* Read up to buflen-1 characters into `buffer`.
+ * A terminating '\0' character is added after the input.  */
+{
+  int old_curs = curs_set(1);
+  int pos = 0;
+  int len = 0;
+  int x, y;
+
+  getyx(win, y, x);
+  for (;;) {
+    int c;
+
+    buffer[len] = ' ';
+    mvwaddnstr(win, y, x, buffer, len+1);
+    wmove(win,y, x+pos);
+    wrefresh(win);
+    c = wgetch(win);
+
+    if(c == KEY_RESIZE){
+      clear();
+      buffer[0] = '\0';
+      break;
+    }
+
+    if (c == KEY_ENTER || c == '\n' || c == '\r') {
+      break;
+    } else if (isprint(c)) {
+      if (pos < buflen-1) {
+        memmove(buffer+pos+1, buffer+pos, len-pos);
+        buffer[pos++] = c;
+        len += 1;
+      } else {
+        beep();
+      }
+    } else if (c == KEY_LEFT) {
+      if (pos > 0) pos -= 1; else beep();
+    } else if (c == KEY_RIGHT) {
+      if (pos < len) pos += 1; else beep();
+    } else if (c == KEY_BACKSPACE) {
+      if (pos > 0) {
+        memmove(buffer+pos-1, buffer+pos, len-pos);
+        pos -= 1;
+        len -= 1;
+      } else {
+        beep();
+      }
+    } else if (c == KEY_DC) {
+      if (pos < len) {
+        memmove(buffer+pos, buffer+pos+1, len-pos-1);
+        len -= 1;
+      } else {
+        beep();
+      }
+    } else {
+      beep();
+    }
+  }
+  buffer[len] = '\0';
+  if (old_curs != ERR) curs_set(old_curs);
+}
+
+#endif
